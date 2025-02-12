6dae2c0d148219d5941f0c7a0e055b593bbb3646
mihow201 <phamtattu201@gmail.com>
2025-02-12 20:12:21 +0700

    1 phần giao diện admin

--- src/main/java/org/example/DAO/Main.java
+++ src/main/java/org/example/DAO/Main.java
@@ -5,8 +5,5 @@ import org.example.VIEW.HomeJFrame;
 public class Main {
     public static void main(String[] args) {
         new HomeJFrame().setVisible(true);
-        // bạn học tôi là lính đánh thuê
-        // try hard again
-        // nó có thay đổi ko 
     }
 }
